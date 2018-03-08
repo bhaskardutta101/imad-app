061ac94d5a159563bc70e3d3f673489d145e9853
bhaskardutta101 <36702791+bhaskardutta101@users.noreply.github.com>
2018-03-09 01:55:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,72 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title : `Article-One`,
+    heading : `articleOne`,
+    date : `8 March 2018`,
+    content : `     <p>
+        Here will be the content for artivcle-one. Here will be the content for artivcle-one. Here will be the content for artivcle-one. Here will be the content for artivcle-one. Here will be the content for artivcle-one.
+     </p>
+    
+     <p>
+        Here will be the content for artivcle-one. Here will be the content for artivcle-one. Here will be the content for artivcle-one. Here will be the content for artivcle-one. Here will be the content for artivcle-one.
+     </p>
+    
+     <p>
+        Here will be the content for artivcle-one. Here will be the content for artivcle-one. Here will be the content for artivcle-one. Here will be the content for artivcle-one. Here will be the content for artivcle-one.
+     </p>`
+    
+};
+
+function createTemplate(data){
+    
+ var title = data.title;
+ var heading = data.heading;
+ var date = data.date;
+ var content = data.content;
+ 
+ 
+var htmlTemplate = `
+    <html>
+
+  <head>
+      ${title}
+      <link href="/ui/style.css" rel="stylesheet" />
+  </head>
+  
+  <body>
+   <div class="container">
+      <div>
+          <a href="/"> Home </a>
+        <hr />  
+      </div>
+     
+    ${heading}
+     
+     <div>
+         ${date}
+     </div>
+    
+     <div>
+    
+        ${content}
+
+    </div>
+  </div>
+</body>
+</html>`  ;
+
+return htmlTemplate;
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function(req, res){
-     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+     res.send(createTemplate(articleOne));
 });
 
 
