08a5642a3baf8a77a0ddf0e347924727b4c5d98d
bhaskardutta101 <36702791+bhaskardutta101@users.noreply.github.com>
2018-03-09 21:24:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,6 +110,14 @@ app.get('/ui/main.js', function (req, res) {
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  
+});
+
+var counter = 0;
+
+app.get('/counter', function(req, res){
+    counter = counter + 1;
+    res.send(counter.toString());
 });
 
 
