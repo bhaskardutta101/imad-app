df0de998a6633fc5b7af6ef1be97909618b6c0b7
bhaskardutta101 <36702791+bhaskardutta101@users.noreply.github.com>
2018-03-09 21:49:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,27 @@
 console.log('Loaded!');
 //change main content
 
+//for counter
+
+var button = document.getElemetById('counter');
+
+button.onClick = function(){
+    // create a request object
+    
+    var request = new XMLHttpRequest();
+    
+    // capture the response and store it ina varianle
+    
+    request.onreadystateChange = function(){
+        if(request.readystate === XMLHttpRequest.DONE){
+            // take some action
+            if(request.status === 200){
+            var counter = request.responseText;
+            var span = document.getElementById('times');
+            span.innerHTML = counter.toString();
+        }
+    }
+    // not done yet
+};
+
+};
