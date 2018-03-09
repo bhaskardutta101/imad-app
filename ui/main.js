55b841dd54fa6f7aa3ef0e62bff7bc4154d596d3
bhaskardutta101 <36702791+bhaskardutta101@users.noreply.github.com>
2018-03-09 20:53:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,13 @@ console.log('Loaded!');
 var element = document.getElementById('maintext');
 
 element.innerHTML = 'new content by innerHTML';
+
+//move image by click
+
+var img = document.getElementById('madi');
+
+img.onClick = function(){
+  
+  img.style.marginLeft = '100px';
+    
+};
