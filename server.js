47ac8a9005792c4e20aac70e52522ce9c2b138ed
bhaskardutta101 <36702791+bhaskardutta101@users.noreply.github.com>
2018-03-10 21:26:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,140 +1,41 @@
-var express = require('express');
-var morgan = require('morgan');
-var path = require('path');
 
-var app = express();
-app.use(morgan('combined'));
+var element = document.getElementById('maintext');
 
-var articles = {
-    
-    'article-one': {
-        
-    tit : `Article-One`,
-    heading : `articleOne`,
-    date : `8 March 2018`,
-    content : `    
-                <p>
-                    Here will be the content for artivcle-one. Here will be the content for artivcle-one. Here will be the content for artivcle-one. Here will be the content for artivcle-one. Here will be the content for artivcle-one.
-                 </p>
-                
-                 <p>
-                    Here will be the content for artivcle-one. Here will be the content for artivcle-one. Here will be the content for artivcle-one. Here will be the content for artivcle-one. Here will be the content for artivcle-one.
-                 </p>
-                
-                 <p>
-                    Here will be the content for artivcle-one. Here will be the content for artivcle-one. Here will be the content for artivcle-one. Here will be the content for artivcle-one. Here will be the content for artivcle-one.
-                 </p>`
-                
-        },
-        
-     'article-two': {
-        tit : `Article-Two`,
-        heading : `articleTwo`,
-        date : `8 March 2018`,
-        content :     
-                `<p>
-                    Here will be the content for artivcle-two.
-                </p>`
-        },
-        
-     'article-three': {
-        tit : `Article-Three`,
-        heading : `articleThree`,
-        date : `8 March 2018`,
-        content : 
-                ` <p>
-                    Here will be the content for artivcle-three.
-                </p>`
-                
-     }
-};
+element.innerHTML = 'New value by innerHTML';
 
-function createTemplate (data)
-{
- var tit = data.tit;
- var heading = data.heading;
- var date = data.date;
- var content = data.content;
- 
- 
-var htmlTemplate = `
-    <html>
+var image = document.getElementById('madi');
 
-  <head>
-      <title> ${tit} </title>
-      <link href="/ui/style.css" rel="stylesheet" />
-  </head>
-  
-  <body>
-   <div class="container">
-      <div>
-          <a href="/"> Home </a>
-        <hr />  
-      </div>
-     
-     <h3>
-    ${heading}
-     </h3>
-     <div>
-         ${date}
-     </div>
-    
-     <div>
-    
-        ${content}
-
-    </div>
-  </div>
-</body>
-</html>`  ;
-
-return htmlTemplate;
+image.onClick = function(){
+    image.style.marginLeft = '100px';
 }
 
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
 
-app.get('/:articleName', function(req, res){
-    //articleName = article-one
+
+
+
+/*
+//for counter
+
+var button = document.getElementById('counter');
+
+button.onClick = function(){
+    // create a request object
     
-    //articles[articleName] = {} content object for article one
+    var request = new XMLHttpRequest();
     
-    var articleName = req.params.articleName;
-     res.send(createTemplate(articles[articleName]));
-     
-});
+    // capture the response and store it ina varianle
+    
+    request.onreadystateChange = function(){
+        if(request.readystate === XMLHttpRequest.DONE){
+            // take some action
+            if(request.status === 200){
+            var counter = request.responseText;
+            var span = document.getElementById('times');
+            span.innerHTML = counter.toString();
+        }
+    }
+    // not done yet
+};
 
-
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-var counter = 0;
-
-app.get('/counter', function(req, res){
-    counter = counter + 1;
-    res.send(counter.toString());
-});
-
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-  
-});
-
-
-
-
-// Do not change port, otherwise your app won't run on IMAD servers
-// Use 8080 only for local development if you already have apache running on 80
-
-var port = 80;
-app.listen(port, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
+};*/
