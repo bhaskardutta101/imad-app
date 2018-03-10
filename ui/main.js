cf0ed32cc35e5fcc2b37a74eff7f40dc0aba2e9c
bhaskardutta101 <36702791+bhaskardutta101@users.noreply.github.com>
2018-03-10 22:30:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,3 +28,27 @@ button.onclick = function(){
     request.send(null);
     
 };
+
+//submit name
+
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submitbutton');
+
+submit.onclick = function() {
+    //make a request to the server
+    
+    
+    
+    //capture a list of names and make it a list
+    
+    var names = ['name1','name2','name2','name4'];
+    var list = '';
+    for(i=0; i<names.length; i++){
+        list+= '<li>' + names[i] + '</li>';
+        
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+    
+}
