322484e330f430afb9bbea7079910c951473b600
bhaskardutta101 <36702791+bhaskardutta101@users.noreply.github.com>
2018-03-10 22:41:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -131,6 +131,20 @@ app.get('/ui/madi.png', function (req, res) {
   
 });
 
+var names = [];
+app.get('/submit-name/:name', function(req, res) {
+    //get the name from the request
+    
+    var name = req.params.name;
+    
+    names.push(name);
+    
+    //JSON: JAVASCRIPT OBJEST NOTATION
+    
+    res.send(JSON.stringify(names));
+    
+});
+
 
 
 
